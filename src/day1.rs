87489d5133c4666d060ae741e596deeb1f01cc10
Andreas Runfalk <andreas@runfalk.se>
2022-12-01 22:32:39 +0100

    Cleaned up day 1 solution

--- src/day1.rs
+++ src/day1.rs
@@ -3,34 +3,25 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-pub fn part_a(item_calories: &[Option<usize>]) -> usize {
-    let mut calories_by_elf = vec![0];
-    for ic in item_calories.iter().copied() {
-        match ic {
-            Some(c) => *calories_by_elf.last_mut().unwrap() += c,
-            None => calories_by_elf.push(0),
-        }
-    }
-    calories_by_elf.into_iter().max().unwrap()
-}
-
-pub fn part_b(item_calories: &[Option<usize>]) -> usize {
-    let mut calories_by_elf = vec![0];
-    for ic in item_calories.iter().copied() {
-        match ic {
-            Some(c) => *calories_by_elf.last_mut().unwrap() += c,
-            None => calories_by_elf.push(0),
-        }
-    }
-    calories_by_elf.sort();
-    calories_by_elf[calories_by_elf.len() - 3..].iter().sum()
-}
-
 pub fn main(path: &Path) -> Result<(usize, Option<usize>)> {
     let file = File::open(path)?;
     let calories = io::BufReader::new(file)
         .lines()
         .map(|lr| Ok(lr?.parse::<usize>().ok()))
         .collect::<Result<Vec<Option<usize>>>>()?;
-    Ok((part_a(&calories), Some(part_b(&calories))))
+
+    let mut calories_by_elf = vec![0];
+    for item_calories in calories.iter().copied() {
+        match item_calories {
+            Some(c) => *calories_by_elf.last_mut().unwrap() += c,
+            None => calories_by_elf.push(0),
+        }
+    }
+
+    calories_by_elf.sort();
+
+    Ok((
+        calories_by_elf.last().copied().unwrap_or(0),
+        Some(calories_by_elf.iter().copied().rev().take(3).sum()),
+    ))
 }
